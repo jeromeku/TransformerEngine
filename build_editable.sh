d560a5f63fae777c248e4fbc66c6ecb28fe397e4
jeromeku <jerome.ku@gmail.com>
2025-10-27 02:02:05 +0000

    add fp4 dispatch

--- build_editable.sh
+++ build_editable.sh
@@ -2,6 +2,9 @@
 # uv pip install torch --torch-backend=cu128
 # uv pip install nvidia-mathdx==25.1.1
 # uv pip install nvshmem4py-cu12
+
+# rm -rf build
+
 REPO_ROOT=`realpath -L .`
 
 export NVSHMEM_HOME=`realpath -L .venv/lib/python3.12/site-packages/nvidia/nvshmem`
@@ -18,7 +21,8 @@ export NVTE_CMAKE_EXTRA_ARGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=1"
 
 export NVTE_FRAMEWORK="pytorch"
-export NVTE_ENABLE_NVSHMEM=1
-export NVTE_CUDA_ARCHS="90;100"
+export NVTE_ENABLE_NVSHMEM=0
+export NVTE_CUDA_ARCHS="90"
+export LD_LIBRARY_PATH=${NVSHMEM_HOME}/lib
 
 uv pip install --no-build-isolation -v --editable ".[pytorch]" 2>&1 | tee _build.log
