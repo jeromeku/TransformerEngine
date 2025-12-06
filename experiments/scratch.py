c53ca981a092bd187765f4d1ccf781dbc65f63c6
jeromeku <jerome.ku@gmail.com>
2025-12-06 01:58:44 +0000

    add for loop qgemm

--- experiments/scratch.py
+++ experiments/scratch.py
@@ -43,6 +43,7 @@ NVFP4_2D_TILE = [
 ]  # symmetric quant for weight tensors and chain-rule consistency
 NVFP4_TILESHAPE = NVFP4_1D_TILE | NVFP4_2D_TILE
 
+
 def blockwise_quantize_nvfp4(x: torch.Tensor, tile_shape: "NVFP4_TILESHAPE"):
     M, N = x.shape
     assert tile_shape in [NVFP4_1D_TILE, NVFP4_2D_TILE]
@@ -60,9 +61,7 @@ def blockwise_quantize_nvfp4(x: torch.Tensor, tile_shape: "NVFP4_TILESHAPE"):
         vec_max = block_amax.repeat_interleave(tile_len_y, dim=0).unsqueeze(-1)
     else:
         x_reshaped = x.view(M, N // tile_len_x, tile_len_x)
-        vec_max = torch.amax(torch.abs(x_reshaped), dim=-1, keepdim=True).to(
-            torch.float32
-        )
+        vec_max = torch.amax(torch.abs(x_reshaped), dim=-1, keepdim=True).to(torch.float32)
 
     x = x.view(M, N // tile_len_x, tile_len_x)
     decode_scale = torch.div(vec_max, FLOAT4_E2M1_MAX)
@@ -109,3 +108,83 @@ def blockwise_quantize_nvfp4(x: torch.Tensor, tile_shape: "NVFP4_TILESHAPE"):
     sx = decode_scale.squeeze(-1)
 
     return qx, sx
+
+
+def cast_from_fp4x2(qx, out_dtype) -> torch.Tensor:
+    pass
+
+
+def gemm(
+    A: torch.Tensor,
+    B: torch.Tensor,
+    out_dtype: torch.dtype = torch.float32,
+) -> torch.Tensor:
+    """GEMM implementation with unquantized data"""
+
+    A = A.to(out_dtype)
+    B = B.to(out_dtype)
+
+    return A @ B
+
+
+def qgemm(
+    self,
+    qx: torch.Tensor,
+    qw: torch.Tensor,
+    out_dtype: torch.dtype,
+    sx: torch.Tensor,
+    sw: torch.Tensor,
+    global_amax_x: torch.Tensor,
+    global_amax_w: torch.Tensor,
+    out: torch.Tensor | None = None,
+    accumulate: bool = False,
+) -> torch.Tensor:
+    """Python implementation of microblock FP4 GEMM."""
+
+    x = cast_from_fp4x2(qx, out_dtype)
+    w = cast_from_fp4x2(qw, out_dtype)
+
+    sx = sx.to(torch.float32)
+    sw = sw.to(torch.float32)
+
+    MAX_NORMS_SQ = FLOAT4_E2M1_MAX * FLOAT4_E2M1_MAX * FLOAT8_E4M3_MAX * FLOAT8_E4M3_MAX
+    alpha = torch.div(global_amax_x * global_amax_w, MAX_NORMS_SQ).squeeze(-1)
+
+    M, K = x.shape
+    N, K_w = w.shape
+    assert K == K_w, "K dimension mismatch between qx and qw"
+
+    grid_k = K // NVFP4_BLOCKSIZE
+
+    assert sx.shape == (
+        M,
+        K // NVFP4_BLOCKSIZE,
+    ), f"sx shape mismatch: expected ({M}, {K // NVFP4_BLOCKSIZE}), got {sx.shape}"
+    assert sw.shape == (
+        N,
+        K // NVFP4_BLOCKSIZE,
+    ), f"sw shape mismatch: expected ({N}, {K // NVFP4_BLOCKSIZE}), got {sw.shape}"
+
+    y = torch.zeros(M, N, dtype=torch.float32, device=qx.device)
+
+    # Below implementation emulates the FP4 tensor core implementation
+    # Each output element (i, j) is fp32 accumulation of (K // block_length) inner products
+    # Each inner product is sx * sw * (1, block_length) x (block_length, 1) with precision in fp32
+    # Then batch the computation in M, N dimension
+    for k in range(grid_k):
+        k_start = k * NVFP4_BLOCKSIZE
+        k_end = k_start + NVFP4_BLOCKSIZE
+
+        qx_block = x[:, k_start:k_end].clone().contiguous()
+        qw_block = w[:, k_start:k_end].clone().contiguous()
+
+        # Extract scaling factors for the current blocks
+        sx_block = sx[:, k]
+        sw_block = sw[:, k]
+
+        y += torch.outer(sx_block, sw_block) * gemm(qx_block, qw_block.T, torch.float32)
+
+    y = alpha * y
+
+    y = y.to(out_dtype)
+    return y
